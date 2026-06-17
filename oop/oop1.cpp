64f92b242c856643a0a39815f30e3a8e5cfc6694
KISLAY KUMAR RANJAN <kislaykumarranjan@KISLAYs-Mac-mini.local>
2026-06-17 11:43:35 +0530

    addition_heap_memory

--- oop/oop1.cpp
+++ oop/oop1.cpp
@@ -23,6 +23,7 @@ class Employee{
 
 int main(){
     // Creating an object of employee class
+    //stack memory allocation for the object obj1 and obj2
     Employee obj1;
     // setting different attributes of object 1 using available methods 
     obj1.setEmployeeName("John");// setting the name of the obj1 to John
@@ -36,7 +37,16 @@ int main(){
     obj2.setEmployeeSalary(60000);
     cout<<"Employee Name: "<<obj2.employeeName<<endl;
     cout<<"Employee Salary: "<<obj2.getSalary()<<endl;  
+    //for heap memory allocation we can use the new keyword to create an object of the class and it will be created in the heap memory and we can access it using a pointer.
+    Employee *obj3 = new Employee();
+    obj3->setEmployeeName("Alice");
+    obj3->setEmployeeSalary(70000);
+    cout<<"Employee Name: "<<obj3->employeeName<<endl;
+    cout<<"Employee Salary: "<<obj3->getSalary()<<endl; 
+    delete obj3; // deallocating the memory allocated for the object obj3 in the heap memory
 
     return 0;
+    //The class by itself doesn't take any memory. It is the object that takes up the memory once initialized.
+    //The two objects (obj1 and obj2) have separate memory allocated for them in the program though they have the same attributes and methods. Because of this reason, an object cannot access the attributes and methods of any other object and vice-versa.
     
 }
